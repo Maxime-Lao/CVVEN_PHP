af7256af895348023bad86efdd4521fd20947e80
Maxime-Lao <58255355+Maxime-Lao@users.noreply.github.com>
2020-01-22 10:21:21 +0100

    Update reservation.php

--- application/views/reservations/reservation.php
+++ application/views/reservations/reservation.php
@@ -11,42 +11,45 @@
     <form method="post">
         
     
-        <h2><center><font size="6"><p style="color: #e89999  ">Formulaire de réservations</p></font></center></h2><br>   
+        <h2><center><font size="6">Formulaire de réservations</font></center></h2>   
  
+        <div class="login-page">
+            <div class="form">
+                <form class="login-form">
+    
+                    <h4>Pension :</h4>
+                    <center><select name="pension_reservation" size="1">
+                    <option>Demi-pension</option>
+                    <option >Complète</option>
+                    </select></center><br>
+    
+                     <hr>
 
-    <hr>  
-    <h4><p style="color:  #ffffff  ">Pension :</p></h4>
-    <center><select name="pension_reservation" size="1">
-        <option>Demi-pension</option>
-        <option >Complète</option>
-    </select></center>
-    <br>
-    <hr>
-
-    <h4><p style="color:  #ffffff  ">Date de début (Samedi au samedi, 1 semaine, pendant les vacances) :</p></h4>
-    <center><input type="date" name="datedebut_reservation" size="50" /></center>
-    <br>
-    <hr>
-    <h4><p style="color:  #ffffff  ">Date de fin :</p></h4>
-<center><input type="date" name="datefin_reservation" size="50" /></center>
-    <br>
-    <hr>
-<h4><p style="color:  #ffffff  ">Nombre de personnes :</p></h4>
-<input type="text" class="feedback-input" name="nbpersonne_reservation" size="50" />
- <br>
-    <hr>
-<h4><p style="color:  #ffffff  ">Ménage fin de séjour :</p></h4>
-<center><p style="color:  #ffffff  ">Oui:
-<input type="radio" name="menage_reservation" value="1" size="50" />&nbsp;&nbsp;&nbsp;
-Non:
-<input type="radio" name="menage_reservation" value="0" size="50" /></center></p>
-<BR><BR>
-
-<div><input type="submit" value="Valider" /></div>
-
+                    <h4>Date de début (Samedi au samedi, 1 semaine, pendant les vacances) :</h4>
+                    <center><input type="date" name="datedebut_reservation" size="50" /></center>
+    
+                     <hr>
+                     <h4>Date de fin :</h4>
+                     <center><input type="date" name="datefin_reservation" size="50" /></center>
+    
+                     <hr>
+                     <h4>Nombre de personnes :</h4>
+                     <input type="text" class="feedback-input" name="nbpersonne_reservation" size="50" />
+ 
+                     <hr>
+                     <h4>Ménage fin de séjour :</h4>
+                     <center>Oui:
+                     <input type="radio" name="menage_reservation" value="1" size="50" />    
+                     Non:
+                     <input type="radio" name="menage_reservation" value="0" size="50" /></center></p>
 
 
-    </form>
+                     <button>Valider</button>
+
+                </form>
+            </form>
+         </div>
+     </div>
        
 </body>
 </html>
