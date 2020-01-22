7a291acd773f7632a6b2af031b8b5c5c950e1df5
Maxime-Lao <58255355+Maxime-Lao@users.noreply.github.com>
2020-01-22 10:23:17 +0100

    Update header.php

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -2,8 +2,8 @@
 <html>
     <head>
         <script src="vendor/bootstrap/js/bootstrap.min.js"></script>    
-        <link rel="stylesheet" href="<?php echo base_url('vendor/bootstrap/css/Formu.css'); ?>"  rel="stylesheet">
-        <link rel="stylesheet" href="<?php echo base_url('vendor/bootstrap/css/form.css'); ?>"  rel="stylesheet">
+        <link rel="stylesheet" href="<?php echo base_url('vendor/bootstrap/css/green.css'); ?>"  rel="stylesheet">
+        <link rel="stylesheet" href="<?php echo base_url('vendor/bootstrap/js/green.js'); ?>"  rel="stylesheet">
         <meta charset="UTF-8">
 
     </head>
