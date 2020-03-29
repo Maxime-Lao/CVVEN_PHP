3086e191978b955d6d58773acccc28077379c48f
Maxime-Lao <58255355+Maxime-Lao@users.noreply.github.com>
2020-03-29 18:49:23 +0200

    Update delete.php

--- application/views/reservations/delete.php
+++ application/views/reservations/delete.php
@@ -6,7 +6,7 @@
     <br><br><br><br><br><br><br><br><br><br><br><br><br><br>
 <h3><center><p style="color: #000000  ">Cette réservation a bien été éffacée!</p></center></h3>
 
-<p><center><?php echo anchor('Reservations/show', 'Revenir'); ?></center></p>
+<p><center><?php echo anchor('Page/index', 'Revenir'); ?></center></p>
 
 </body>
 </html>
