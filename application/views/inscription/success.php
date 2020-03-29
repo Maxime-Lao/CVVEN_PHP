6aabbdbfc966545f1a8033cecc07a700250feb11
Maxime-Lao <58255355+Maxime-Lao@users.noreply.github.com>
2020-03-29 18:46:57 +0200

    Update success.php

--- application/views/inscription/success.php
+++ application/views/inscription/success.php
@@ -1,13 +1,1 @@
-<html>
-    <head>
-        <title>Inscription</title>
-    </head>
-    <body>
-
-        <h3>Votre compte a été enregistré avec succès ! ٩(๑`^´๑)۶</h3>
-        
-        <p><div><a href="/CVVEN/index.php/verifco/connexion"><input type="submit" value="Se connecter" /></a></div></p>
-
-        <p><?php echo anchor('Verif/create', 'Try it again!'); ?></p>
-    </body>
-</html>
+<?php redirect('page'); ?>
