66ab34f2a31e163c3bf6d513a38855df7754fa73
Maxime-Lao <58255355+Maxime-Lao@users.noreply.github.com>
2020-03-29 18:49:47 +0200

    Create deleteclient.php

---
+++ application/views/reservations/deleteclient.php
@@ -0,0 +1,12 @@
+<html>
+<head>
+<title>Client</title>
+</head>
+<body>
+    <br><br><br><br><br><br><br><br><br><br><br><br><br><br>
+<h3><center><p style="color: #000000  ">Ce client a bien été éffacé!</p></center></h3>
+
+<p><center><?php echo anchor('Page/index', 'Revenir'); ?></center></p>
+
+</body>
+</html>
