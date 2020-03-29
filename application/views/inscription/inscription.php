138107a18d1aacdb83b1055f5048345d57c01e4f
Maxime-Lao <58255355+Maxime-Lao@users.noreply.github.com>
2020-03-29 18:46:32 +0200

    Update inscription.php

--- application/views/inscription/inscription.php
+++ application/views/inscription/inscription.php
@@ -1,10 +1,16 @@
-        <title>Insciption</title>
-
+<html>
+<head>
+<title>Inscription</title>
+<link href="<?php echo base_url('assets/css/bootstrap.min.css');?>" rel="stylesheet">
+</head>
+    <body>  
         <?php echo validation_errors(); ?>
 
         <?php echo form_open('verif/create'); ?>
-            
-
+        <form method="post">
+        <div class="container">
+            <div class="col-md-4 col-md-offset-4">   
+            <center><h2 class="form-signin-heading">Inscription</h2><br></center>
             <h5>Nom :</h5>
             <input type="text" name="nom_client" value="" size="50" />
 
@@ -28,8 +34,12 @@
             
             <br/>
             <br/>
-            <div><input type="submit" value="Valider" /></div>
+            <div><input type="submit" class="btn btn-lg btn-primary btn-block" value="Valider" /></div>
+            </div>
+        </div>    
         </form>
+    </body>
+</html>
 
 
     
