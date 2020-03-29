26100262459d6e4cbbb04356043be3432f38d6fc
Maxime-Lao <58255355+Maxime-Lao@users.noreply.github.com>
2020-03-29 18:51:04 +0200

    Update show.php

--- application/views/reservations/show.php
+++ application/views/reservations/show.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
     <head>
+
     </head>
     <body>
 
@@ -13,22 +14,26 @@
                 <th>Date de fin</th>
                 <th>Nombre de personnes</th>
                 <th>Menage</th>
+                <th>Verification</th>
                 <th>Effacer</th>
+                <th>Modifier</th>
             </tr>
             <?php foreach ($posts as $post) { ?>
             <tr><td><p style="color:  #000000   "><?= $post->id_reservation ?></p></td>
                     <td><p style="color:  #000000   "><?= $post->pension_reservation ?></p>
                     </td><td><p style="color:  #000000   "><?= $post->datedebut_reservation ?></p>
                     </td><td><p style="color:  #000000   "><?= $post->datefin_reservation ?></p></td>
-                    <td><p style="color:  #000000   "><?= $post->nbpersonne_reservation ?></p></td>
+                    <td><p style="color:  #000000   "><?= $post->nbpersonne_reservation?></p></td>
                     <td><p style="color:  #000000   "><?php
                         if ($post->menage_reservation == 1) {
                             echo "oui";
                         } else {
                             echo "non";
                         }
-                        ?></td>
+                    ?></td>
+                    <td><p style="color:  #000000   "><?= $post->verif_reservation ?></p></td>
                     <td><a href='delete?id=<?=$post->id_reservation ?>'>Delete</a></td>
+                    <td><a href='update?id=<?=$post->id_reservation ?>'>Update</a></td>
                     </tr>
 
             <?php } ?>  
